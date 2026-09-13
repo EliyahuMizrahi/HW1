3048b57a08c5bbcf3fb9051cc487c290655d3260
Eliyahu Mizrahi <eliyahumizrahi123@gmail.com>
2026-09-13 19:01:33 -0400

    Add InfoGet.py for process information retrieval; remove hello.py

---
+++ InfoGet.py
@@ -0,0 +1,10 @@
+import os
+
+if __name__ == "__main__":
+    print(os.getpid())
+    print(os.getppid())
+
+    for key, value in os.environ.items():
+        print(f"{key}={value}")
+
+    print(os.listdir('/proc/self/fd'))
